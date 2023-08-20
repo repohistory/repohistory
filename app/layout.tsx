6ab2da51f0238b6f5e4a37e10c401785b2896d50
Max Shen <m4xshen@gmail.com>
2023-08-20 11:44:01 +0800

    feat: basic wellcome page

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-black min-h-screen`}>
         <Providers>
           {children}
         </Providers>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "@nextui-org/button"
-
 export default function Home() {
   return (
-    <Button>Click Me</Button>
+    <h1 className="pt-14 text-center text-7xl font-bold text-white">
+      repohistory
+    </h1>
   )
 }
