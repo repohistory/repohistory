f38bb5946af609e8c56bc084e730a5ad0c7c3c0e
Max Shen <m4xshen@gmail.com>
2023-08-20 14:04:35 +0800

    fix: lint error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Providers } from './providers';
+import Providers from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,6 @@
 
 import { NextUIProvider } from '@nextui-org/react';
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return <NextUIProvider>{children}</NextUIProvider>;
 }
