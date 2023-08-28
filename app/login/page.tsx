3ddf2699ad8952d42051b3d0c2c6225a4799ca44
Max Shen <m4xshen@gmail.com>
2023-08-28 09:44:47 +0800

    feat: login and get access token

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,36 @@
 'use client';
 
 import { Button } from '@nextui-org/button';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function LoginPage() {
+  const searchParams = useSearchParams();
+  const code = searchParams.get('code');
+
+  useEffect(() => {
+    if (code) {
+      (async () => {
+        const response = await fetch(
+          `https://github.com/login/oauth/access_token?${new URLSearchParams({
+            code,
+            client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+            client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+          })}`,
+          {
+            headers: {
+              Accept: 'application/json',
+            },
+            method: 'POST',
+          },
+        );
+        const data = await response.json();
+        console.log(data.access_token);
+      })();
+    }
+  }, [code]);
+
   return (
     <div className="flex flex-col items-center gap-10">
       <div>
@@ -14,7 +42,18 @@ export default function LoginPage() {
         </h2>
       </div>
       <div>
-        <Button className="bg-[#1f6feb] text-white">GitHub Login</Button>
+        <Button className="bg-[#1f6feb] text-white">
+          <Link
+            href={`https://github.com/login/oauth/authorize?${new URLSearchParams(
+              {
+                client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+                redirect_url: 'http://localhost:3000/login',
+              },
+            )}`}
+          >
+            GitHub Login
+          </Link>
+        </Button>
       </div>
     </div>
   );
---
+++ environment.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_CLIENT_ID: string;
+    NEXT_PUBLIC_CLIENT_SECRET: string;
+  }
+}
