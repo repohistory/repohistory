cdbb5cdb7606ef954697c43bf8b41bd6696e3a6f
Max Shen <m4xshen@gmail.com>
2023-11-08 20:24:11 +0800

    fix: setting page data repo

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -6,7 +6,7 @@ import useDataRepo from '@/hooks/useDataRepo';
 
 export default function SettingsPage() {
   const { dataRepo }: { dataRepo: any } = useDataRepo();
-  const branches = useBranches(dataRepo?.full_name ?? []);
+  const branches = useBranches(dataRepo ?? []);
 
   return (
     <div className="mx-64 mt-20 flex flex-col gap-5">
