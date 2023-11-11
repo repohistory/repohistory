904f3c226e62f5ec99885ba44f178305f0846362
Max Shen <m4xshen@gmail.com>
2023-11-11 17:23:08 +0800

    chore: remove unused code

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,18 +1,9 @@
-'use client';
-
-import BranchSelector from '@/components/BranchSelector';
-import useBranches from '@/hooks/useBranches';
-import useDataRepo from '@/hooks/useDataRepo';
 import { Button, Link } from '@nextui-org/react';
 
 export default function SettingsPage() {
-  const { dataRepo }: { dataRepo: any } = useDataRepo();
-  const branches = useBranches(dataRepo ?? []);
-
   return (
     <div className="mx-64 mt-20 flex flex-col items-start gap-5 pb-5">
       <h1 className="text-3xl font-bold text-white">Settings</h1>
-      {branches?.length ? <BranchSelector branches={branches} /> : null}
       <Button
         href="https://github.com/apps/repohistory/installations/new"
         target="_blank"
@@ -21,7 +12,7 @@ export default function SettingsPage() {
         radius="sm"
         className="bg-[#222222] text-white"
       >
-        Configure data repos
+        Configure selected repositories
       </Button>
     </div>
   );
