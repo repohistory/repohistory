8761bcf06dd6f987ddcad16b9796ce5ab440d616
Max Shen <m4xshen@gmail.com>
2023-11-20 17:01:46 +0800

    docs: refine slogan

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import { Inter } from 'next/font/google';
 import Providers from '@/app/providers';
 
 export const metadata: Metadata = {
-  title: 'Repohistory',
+  title: 'Repohistory - A GitHub repo traffic history tracker',
   description:
-    'Repohistory is a GitHub repo traffic history tracker that keeps records beyond the standard 14 days, offering an easy and detailed long-term view.',
+    'Repohistory is an open-source platform for tracking GitHub repo traffic history longer than 14 days, offering an easy and detailed long-term view.',
 };
 
 const inter = Inter({ subsets: ['latin'] });
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} min-h-screen bg-[#0A0A0B] dark no-scrollbar`}
+        className={`${inter.className} no-scrollbar min-h-screen bg-[#0A0A0B] dark`}
       >
         <Providers>{children}</Providers>
       </body>
