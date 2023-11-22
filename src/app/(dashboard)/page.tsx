1515fe8c11a59d53c2f0e2cbd91f85e71127edbd
Max Shen <m4xshen@gmail.com>
2023-11-22 11:05:16 +0800

    feat: refine installation instruction

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable no-restricted-syntax */
 import RepoCard from '@/components/RepoCard';
-import { Link } from '@nextui-org/react';
+import { Image, Link } from '@nextui-org/react';
 
 import { fetchInstallationIds } from '@/utils/dbHelpers';
 import { cookies } from 'next/headers';
@@ -36,23 +36,56 @@ export default async function Dashboard() {
   let content;
   if (repos.length === 0 || repos.length > limit) {
     content = (
-      <div className="flex h-full w-full flex-col items-center justify-center">
-        <h1 className="text-center text-2xl font-bold text-white">
-          {repos.length === 0 ? 'No' : `More than ${limit}`} repositories
-          selected
-        </h1>
-        <p className="mt-2 text-center text-sm text-gray-500">
-          Please{' '}
-          <Link
-            underline="always"
-            isExternal
-            className="text-sm"
-            href="https://github.com/apps/repohistory/installations/new"
-          >
-            {repos.length === 0 ? 'install' : 'configure'} GitHub App
-          </Link>{' '}
-          and select at most {limit} repositories you want to track.
-        </p>
+      <div className="flex w-full flex-col items-center justify-center gap-5 text-white md:flex-row md:items-start">
+        <div className="flex flex-col justify-start gap-3">
+          <h1 className="text-2xl font-bold">
+            {repos.length === 0
+              ? 'No repositories selected'
+              : `More than ${limit} repositories selected`}
+          </h1>
+          <div className="mt-2">
+            Visit <Link
+              underline="always"
+              isExternal
+              className="text-sm"
+              href="https://github.com/apps/repohistory/installations/new"
+            >
+              GitHub App page
+            </Link> and follow the instructions.
+          </div>
+          <div className="flex gap-2">
+            <div className="flex h-7 w-7 items-center justify-center rounded-full bg-white text-black">
+              1
+            </div>
+            <span>
+              Check <b>Only select repositories</b>
+            </span>
+          </div>
+          <div className="flex gap-2">
+            <div className="flex h-7 w-7 items-center justify-center rounded-full bg-white text-black">
+              2
+            </div>
+            <span>
+              Select <b>at most {limit}</b> repositories you want to track
+            </span>
+          </div>
+          <div className="flex gap-2">
+            <div className="flex h-7 w-7 items-center justify-center rounded-full bg-white text-black">
+              3
+            </div>
+            <span>
+              Click {repos.length === 0 ? 'Install' : 'Save'} and refresh this
+              page
+            </span>
+          </div>
+        </div>
+        <Image
+          src="https://github.com/m4xshen/img-host/assets/74842863/8156efbd-7be0-4432-8f26-8489e0abd782"
+          alt="GitHub App installation instruction"
+          width={350}
+          radius="md"
+          className="flex-shrink-0"
+        />
       </div>
     );
   } else {
