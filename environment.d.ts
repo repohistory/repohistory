19ad9e105a4883850901679c45933a2a7d98fc7d
Max Shen <m4xshen@gmail.com>
2023-11-22 20:50:44 +0800

    feat: verify webhook secret

--- environment.d.ts
+++ environment.d.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 namespace NodeJS {
   interface ProcessEnv {
+    NEXT_PUBLIC_GITHUB_WEBHOOK_SECRET: string;
     NEXT_PUBLIC_GA_MEASUREMENT_ID: string;
     NEXT_PUBLIC_CLIENT_ID: string;
     NEXT_PUBLIC_APP_ID: string;
--- src/app/api/github-webhook/route.ts
+++ src/app/api/github-webhook/route.ts
@@ -2,6 +2,21 @@ import createInstallation from '@/services/createInstallation';
 import deleteInstallation from '@/services/deleteInstallation';
 import updateTraffic from '@/services/updateTraffic';
 import { headers } from 'next/headers';
+import * as crypto from 'crypto';
+
+const WEBHOOK_SECRET = process.env.NEXT_PUBLIC_GITHUB_WEBHOOK_SECRET;
+
+const verifySignature = async (body: any, sig: any) => {
+  const signature = crypto
+    .createHmac('sha256', WEBHOOK_SECRET)
+    .update(JSON.stringify(body))
+    .digest('hex');
+
+  const trusted = Buffer.from(`sha256=${signature}`, 'ascii');
+  const untrusted = Buffer.from(sig);
+
+  return crypto.timingSafeEqual(trusted, untrusted);
+};
 
 // eslint-disable-next-line import/prefer-default-export
 export async function POST(req: Request) {
@@ -14,6 +29,16 @@ export async function POST(req: Request) {
   }
 
   const res = await req.json();
+
+  if (!verifySignature(res, headersList.get('x-hub-signature-256'))) {
+    console.log('Invalid signature');
+    return new Response('', {
+      status: 401,
+    });
+  }
+
+  console.log('Valid signature');
+
   const installationId = res.installation.id;
 
   if (event === 'installation') {
