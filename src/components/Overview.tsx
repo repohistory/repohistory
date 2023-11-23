9302f632420c7deb3c5e2bbe8be37b363532d612
Max Shen <m4xshen@gmail.com>
2023-11-23 14:00:27 +0800

    feat: refine total counts display method

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -13,52 +13,61 @@ export default async function Overview({
   clonesTotal: number;
 }) {
   return (
-    <div
-      className="flex flex-col gap-4 rounded-medium border
-        border-[#303031] bg-[#111112] p-5 text-white xl:w-1/3"
-    >
-      <h1 className="text-3xl font-bold">{repo?.name}</h1>
-      <div>{repo?.description}</div>
-      <div className="flex gap-1">
-        <Link
-          isExternal
-          isBlock
-          color="foreground"
-          href={`${repo?.html_url}/stargazers`}
-          className="flex items-center gap-2 text-white"
-        >
-          <Star />
-          {repo?.stargazers_count}
-        </Link>
-        <Link
-          isExternal
-          isBlock
-          color="foreground"
-          href={`${repo?.html_url}/forks`}
-          className="flex items-center gap-2 text-white"
-        >
-          <Fork />
-          {repo?.forks_count}
-        </Link>
-        <Link
-          isExternal
-          isBlock
-          color="foreground"
-          href={`${repo?.html_url}/issues`}
-          className="flex items-center gap-2 text-white"
-        >
-          <Issue />
-          {repo?.open_issues_count}
-        </Link>
-      </div>
-      <div className="mt-8 flex justify-center gap-10">
-        <div>
-          <div className="text-center font-semibold ">Total Clones</div>
-          <div className="text-center text-4xl font-bold">{clonesTotal}</div>
+    <div className="flex flex-col gap-5 xl:w-1/3">
+      <div
+        className="flex h-2/3 flex-col gap-4 rounded-medium border
+        border-[#303031] bg-[#111112] p-5 text-white"
+      >
+        <h1 className="text-3xl font-bold">{repo?.name}</h1>
+        <div>{repo?.description}</div>
+        <div className="flex gap-1">
+          <Link
+            isExternal
+            isBlock
+            color="foreground"
+            href={`${repo?.html_url}/stargazers`}
+            className="flex items-center gap-2 text-white"
+          >
+            <Star />
+            {repo?.stargazers_count}
+          </Link>
+          <Link
+            isExternal
+            isBlock
+            color="foreground"
+            href={`${repo?.html_url}/forks`}
+            className="flex items-center gap-2 text-white"
+          >
+            <Fork />
+            {repo?.forks_count}
+          </Link>
+          <Link
+            isExternal
+            isBlock
+            color="foreground"
+            href={`${repo?.html_url}/issues`}
+            className="flex items-center gap-2 text-white"
+          >
+            <Issue />
+            {repo?.open_issues_count}
+          </Link>
         </div>
-        <div>
+      </div>
+
+      <div className="flex h-1/3 gap-5">
+        <div
+          className="flex w-1/2 flex-col justify-center gap-3
+            rounded-medium border border-[#303031] bg-[#111112] p-5 text-white"
+        >
+          <div className="text-center font-semibold ">Total Clones</div>
+          <div className="text-center text-3xl font-bold">{clonesTotal}</div>
+        </div>
+        <div
+          className="flex w-1/2 flex-col justify-center gap-3
+            rounded-medium border border-[#303031] bg-[#111112] p-5 text-white"
+        >
           <div className="text-center font-semibold">Total Views</div>
-          <div className="text-center text-4xl font-bold">{viewsTotal}</div>
+          <div className="text-center text-3xl font-bold">{viewsTotal}</div>
         </div>
       </div>
     </div>
