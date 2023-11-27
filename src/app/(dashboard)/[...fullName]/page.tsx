05fad7780b2446fb4d1da4cf0fdc323e864bd024
Max Shen <m4xshen@gmail.com>
2023-11-27 13:35:59 +0800

    refactor: extract popular charts

--- src/app/(dashboard)/[...fullName]/page.tsx
+++ src/app/(dashboard)/[...fullName]/page.tsx
@@ -5,86 +5,46 @@ import { redirect } from 'next/navigation';
 import Overview from '@/components/Overview';
 import TrafficCharts from '@/components/TrafficCharts';
 import LineChart from '@/components/LineChart';
-import DoughnutChart from '@/components/DoughnutChart';
+import PopularCharts from '@/components/PopularCharts';
 import { app } from '@/utils/octokit';
 import getInstallationIds from '@/utils/getInstallationIds';
-
-const colors = ['#62C3F8', '#4F9BC4', '#3A7391', '#264B5E'];
 
 export default async function RepoPage({
   params,
 }: {
   params: { fullName: string };
 }) {
-  const fullName = `${params.fullName[0]}/${params.fullName[1]}`;
-
-  let siteLabels = [];
-  let contentLabels = [];
-
   const installationIds = await getInstallationIds();
-
+  const fullName = `${params.fullName[0]}/${params.fullName[1]}`;
   const fetchPromises: Promise<any>[] = [];
   let repo = null;
+  let octokit = null;
+
   for await (const installationId of installationIds) {
-    try {
-      const octokit = await app.getInstallationOctokit(installationId);
+    octokit = await app.getInstallationOctokit(installationId);
 
-      const test = await octokit.request(`GET /installation/repositories`);
+    const {
+      data: { repositories: repos },
+    } = await octokit.request(`GET /installation/repositories`);
 
-      repo = test.data.repositories.find(
-        (repository: any) => repository.full_name === fullName,
-      );
+    repo = repos.find((repository) => repository.full_name === fullName);
 
-      if (!repo) {
-        // eslint-disable-next-line no-continue
-        continue;
-      }
-
-      const { data: sites } = await octokit.request(
-        `GET /repos/${repo.full_name}/traffic/popular/referrers`,
-      );
-      siteLabels = sites.slice(0, 4).map((site: any, index: number) => ({
-        name: site.referrer,
-        count: site.count,
-        uniques: site.uniques,
-        color: colors[index],
-      }));
-
-      const { data: contents } = await octokit.request(
-        `GET /repos/${repo.full_name}/traffic/popular/paths`,
-      );
-      contentLabels = contents
-        .slice(0, 4)
-        .map((content: any, index: number) => {
-          const truncatedTitle = content.title;
-          return {
-            name: truncatedTitle,
-            path: `https://github.com${content.path}`,
-            count: content.count,
-            uniques: content.uniques,
-            color: colors[index],
-          };
-        });
-
-      const totalStars = Math.ceil(repo.stargazers_count / 100);
-      for (let page = 1; page <= totalStars; page += 1) {
-        fetchPromises.push(
-          octokit.request(`GET /repos/${repo.full_name}/stargazers`, {
-            per_page: 100,
-            page,
-            headers: {
-              accept: 'application/vnd.github.v3.star+json',
-            },
-          }),
-        );
-      }
-
+    if (repo) {
       break;
-    } catch (error) {
-      console.error(
-        'Error fetching repository data for installation ID:',
-        installationId,
-        error,
+    }
+  }
+
+  if (octokit && repo) {
+    const totalStars = Math.ceil(repo.stargazers_count / 100);
+    for (let page = 1; page <= totalStars; page += 1) {
+      fetchPromises.push(
+        octokit.request(`GET /repos/${repo.full_name}/stargazers`, {
+          per_page: 100,
+          page,
+          headers: {
+            accept: 'application/vnd.github.v3.star+json',
+          },
+        }),
       );
     }
   }
@@ -103,8 +63,7 @@ export default async function RepoPage({
         <TrafficCharts fullName={fullName} />
       </div>
       <div className="flex w-full flex-col gap-5 xl:flex-row">
-        <DoughnutChart title="Referring Sites" labels={siteLabels} />
-        <DoughnutChart title="Popular Content" labels={contentLabels} />
+        <PopularCharts octokit={octokit} fullName={repo.full_name} />
       </div>
     </div>
   );
