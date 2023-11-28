049846540ef229d2918ed07b7e00275a2f72a387
Max Shen <m4xshen@gmail.com>
2023-11-28 13:13:36 +0800

    fix: apple touch icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,31 @@ export default function RootLayout({
           crossOrigin="use-credentials"
           href="/manifest.json"
         />
+        <meta name="application-name" content="Repohistory" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Repohistory" />
+        <meta
+          name="description"
+          content="An open-source dashboard for tracking GitHub repo traffic history longer than 14 days."
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#0A0A0B" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#0A0A0B" />
+
+        <link rel="apple-touch-icon" href="/icons/512x512.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/180x180.png"
+        />
       </head>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
