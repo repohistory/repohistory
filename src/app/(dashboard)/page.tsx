7e38adc35b3506b43edcf1a8bb7925847e896d52
Max Shen <m4xshen@gmail.com>
2024-01-08 20:29:07 +0800

    revert: add product hunt cta
    This reverts commit 354ccfc0a54275a4fa985af1adf49dfb9763128c.

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -5,7 +5,6 @@ import Guide from '@/components/Guide';
 import getRepos from '@/utils/getRepos';
 import getInstallationIds from '@/utils/getInstallationIds';
 import { getLimit } from '@/utils/getLimit';
-import { Link } from '@nextui-org/react';
 
 export default async function Dashboard() {
   const installationIds = await getInstallationIds();
@@ -14,27 +13,18 @@ export default async function Dashboard() {
   const isValidInstallation = repos.length > 0 && repos.length <= limit;
 
   return (
-    <div className="flex flex-col items-center pt-5">
-      <Link
-        isExternal
-        href="https://www.producthunt.com/posts/repohistory?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-repohistory"
-        className="font-semibold text-white underline"
-      >
-        🎉 Help us grow on Product Hunt 🎉
-      </Link>
-      <div className="flex w-full justify-center px-5 py-5 md:px-10 lg:px-20 ">
-        {isValidInstallation ? (
-          <div className="grid w-full gap-5 md:grid-cols-2 xl:grid-cols-3">
-            {repos
-              .sort((a, b) => b.stargazers_count - a.stargazers_count)
-              .map((repo: any) => (
-                <RepoCard repo={repo} key={repo.id} />
-              ))}
-          </div>
-        ) : (
-          <Guide repos={repos} limit={limit} />
-        )}
-      </div>
+    <div className="flex w-full justify-center px-5 py-5 sm:py-10 md:px-10 lg:px-20 ">
+      {isValidInstallation ? (
+        <div className="grid w-full gap-5 md:grid-cols-2 xl:grid-cols-3">
+          {repos
+            .sort((a, b) => b.stargazers_count - a.stargazers_count)
+            .map((repo: any) => (
+              <RepoCard repo={repo} key={repo.id} />
+            ))}
+        </div>
+      ) : (
+        <Guide repos={repos} limit={limit} />
+      )}
     </div>
   );
 }
