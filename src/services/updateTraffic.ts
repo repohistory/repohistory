c99cb3c89dacd7f79c30d90c7d2a10b20043978f
Max Shen <m4xshen@gmail.com>
2024-09-04 20:41:14 +0800

    fix: catch error

--- src/services/updateTraffic.ts
+++ src/services/updateTraffic.ts
@@ -16,47 +16,51 @@ export default async function updateTraffic(installationId: number) {
 
   for (const repo of repos) {
     console.log(repo.full_name, 'start');
-    const { data: viewsData } = await octokit.request(
-      `GET /repos/${repo.full_name}/traffic/views`,
-    );
-
-    const { data: clonesData } = await octokit.request(
-      `GET /repos/${repo.full_name}/traffic/clones`,
-    );
-
-    // Insert or update views data
-    for (const view of viewsData.views) {
-      await supabase.from('repository_traffic').upsert(
-        [
-          {
-            full_name: repo.full_name,
-            date: view.timestamp,
-            views_count: view.count,
-            unique_views_count: view.uniques,
-          },
-        ],
-        {
-          onConflict: 'full_name, date',
-        },
+    try {
+      const { data: viewsData } = await octokit.request(
+        `GET /repos/${repo.full_name}/traffic/views`,
       );
-    }
 
-    // Insert or update clones data
-    for (const clone of clonesData.clones) {
-      await supabase.from('repository_traffic').upsert(
-        [
-          {
-            full_name: repo.full_name,
-            date: clone.timestamp,
-            clones_count: clone.count,
-            unique_clones_count: clone.uniques,
-          },
-        ],
-        {
-          onConflict: 'full_name, date',
-        },
+      const { data: clonesData } = await octokit.request(
+        `GET /repos/${repo.full_name}/traffic/clones`,
       );
+
+      // Insert or update views data
+      for (const view of viewsData.views) {
+        await supabase.from('repository_traffic').upsert(
+          [
+            {
+              full_name: repo.full_name,
+              date: view.timestamp,
+              views_count: view.count,
+              unique_views_count: view.uniques,
+            },
+          ],
+          {
+            onConflict: 'full_name, date',
+          },
+        );
+      }
+
+      // Insert or update clones data
+      for (const clone of clonesData.clones) {
+        await supabase.from('repository_traffic').upsert(
+          [
+            {
+              full_name: repo.full_name,
+              date: clone.timestamp,
+              clones_count: clone.count,
+              unique_clones_count: clone.uniques,
+            },
+          ],
+          {
+            onConflict: 'full_name, date',
+          },
+        );
+      }
+      console.log(repo.full_name, 'done');
+    } catch (error) {
+      console.log(repo.full_name, 'error', error);
     }
-    console.log(repo.full_name, 'done');
   }
 }
