7e9b81a1942007d320b3db94ec74dc049eafbfd6
Henri <henri.de.boutray@colibritd.com>
2025-01-15 16:03:10 +0100

    BarChart downloadable data

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,15 +1,16 @@
 'use client';
 
+import Download from '@/components/Icons/Download';
 import { barOptions } from '@/utils/chartjs';
-import { Chip, Spinner } from '@nextui-org/react';
+import { Chip, Link, Spinner } from '@nextui-org/react';
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
   BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
   Title,
   Tooltip,
-  Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 
@@ -38,6 +39,16 @@ export default function BarChart({
   secondaryLabel,
   data,
 }: Props) {
+  const downloadableData: Record<string, any> = {};
+  if (data !== null) {
+    for (let i = 0; i < data.labels.length!; i += 1) {
+      const ithData: Record<string, any> = {};
+      data.datasets.forEach((dataset) => {
+        ithData[dataset.label] = data.datasets[i].data;
+      });
+      downloadableData[data.labels[i]] = ithData;
+    }
+  }
   return (
     <div
       className="flex flex-col items-center rounded-medium
@@ -49,25 +60,38 @@ export default function BarChart({
       ) : (
         <Spinner color="primary" className="py-10" />
       )}
-      <div className="mt-1 flex justify-center gap-3">
-        <Chip
-          color="primary"
-          variant="dot"
-          classNames={{
-            base: 'border border-[#303031]',
-          }}
+      <div className="relative mt-1 w-full">
+        <div className="flex justify-center gap-3">
+          <Chip
+            color="primary"
+            variant="dot"
+            classNames={{
+              base: 'border border-[#303031]',
+            }}
+          >
+            {primaryLabel}
+          </Chip>
+          <Chip
+            color="secondary"
+            variant="dot"
+            classNames={{
+              base: 'border border-[#303031]',
+            }}
+          >
+            {secondaryLabel}
+          </Chip>
+        </div>
+        <Link
+          href={`data:text/json;charset=utf-8,${encodeURIComponent(
+            JSON.stringify(downloadableData),
+          )}`}
+          download="repo-data.json"
+          isBlock
+          className="items-right absolute right-0 flex gap-2 text-white"
         >
-          {primaryLabel}
-        </Chip>
-        <Chip
-          color="secondary"
-          variant="dot"
-          classNames={{
-            base: 'border border-[#303031]',
-          }}
-        >
-          {secondaryLabel}
-        </Chip>
+          <Download />
+          Download raw data
+        </Link>
       </div>
     </div>
   );
