d5d1ae8f525ed0c2ee58b1c4f5903be39ef5b16d
Max Shen <m4xshen@gmail.com>
2025-06-23 18:58:35 +0800

    feat: loading when sign in

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { GitHub } from "@/components/icons";
 import { signin } from "@/actions/auth";
+import { useState } from "react";
+import { Loader2Icon } from "lucide-react"
 
 export default function SignInPage() {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-background">
       <div className="max-w-md space-y-8 p-8">
@@ -20,8 +26,16 @@ export default function SignInPage() {
             Repohistory
           </h1>
         </div>
-        <Button className="w-full" onClick={signin}>
-          <GitHub />
+        <Button className="w-full" onClick={() => {
+          setIsLoading(true);
+          signin()
+        }}>
+          {
+            isLoading ?
+              <Loader2Icon className="animate-spin" />
+              :
+              <GitHub />
+          }
           Continue with GitHub
         </Button>
       </div>
