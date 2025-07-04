d55f8dac72f85c59c4d947370ebce3149ea6078c
Max Shen <m4xshen@gmail.com>
2025-07-04 22:47:59 +0800

    fix: add back supabase client

--- src/utils/update-traffic.ts
+++ src/utils/update-traffic.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 
 export async function updateTraffic(installationId: number) {
   const supabase = createClient(
-    process.env.SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
 
