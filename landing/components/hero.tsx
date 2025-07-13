e4959dcfa976c1360672061562fd0897c741f96b
Max Shen <m4xshen@gmail.com>
2025-07-13 21:52:17 +0800

    feat(landing): star on github link

--- landing/components/hero.tsx
+++ landing/components/hero.tsx
@@ -2,6 +2,7 @@ import { BlurFade } from "@/components/magicui/blur-fade";
 import { ImageTiltWrapper } from "@/components/image-tilt-wrapper";
 import Image from "next/image";
 import Link from "next/link";
+import { ChevronRight } from "lucide-react";
 
 export function Hero() {
   return (
@@ -13,13 +14,24 @@ export function Hero() {
         <h2 className="text-lg font-medium text-neutral-300 max-w-lg">
           Track your repository traffic history longer than 14 days with beautiful visualizations
         </h2>
-        <Link
-          href="https://app.repohistory.com"
-          className="bg-white text-black px-4 py-2 hover:brightness-100 brightness-90 active:scale-[0.98] transition-all rounded-lg font-medium inline-block"
-          role="button"
-        >
-          Start tracking
-        </Link>
+        <div className="flex gap-4 flex-col sm:flex-row">
+          <Link
+            href="https://app.repohistory.com"
+            className="bg-white text-black px-4 py-2 hover:brightness-100 brightness-90 active:scale-[0.98] transition-all rounded-lg font-medium inline-block"
+            role="button"
+          >
+            Start tracking
+          </Link>
+          <Link
+            href="https://github.com/repohistory/repohistory"
+            className="text-white px-4 py-2 hover:bg-neutral-900/60 active:scale-[0.98] transition-all rounded-lg font-medium inline-flex items-center gap-1"
+            role="button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Star on GitHub <ChevronRight className="size-5" />
+          </Link>
+        </div>
       </div>
       <div className="bg-[#62C3F8] -translate-x-1/2 absolute inset-0 top-2/5 left-1/2 h-1/4 w-3/5 blur-[9rem] opacity-0 animate-[fadeIn_2s_ease-in-out_forwards] md:h-1/3"></div>
       <div className="relative">
