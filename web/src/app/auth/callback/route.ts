c01ad94eef5a0ad0e6715462123ce334a56efa94
Max Shen <m4xshen@gmail.com>
2026-02-20 14:28:07 -0600

    fix(web): replace origin with env var

--- web/src/app/auth/callback/route.ts
+++ web/src/app/auth/callback/route.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers'
 import { createClient } from '@/utils/supabase/server'
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url)
+  const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
   let next = searchParams.get('next') ?? '/'
@@ -37,9 +37,9 @@ export async function GET(request: Request) {
         })
       }
 
-      return NextResponse.redirect(`${origin}${next}`)
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}${next}`)
     }
   }
 
-  return NextResponse.redirect(`${origin}/`)
+  return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SITE_URL}/`)
 }
